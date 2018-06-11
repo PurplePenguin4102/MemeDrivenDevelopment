e41b6bfad90b6d6a090c222636b238b396c856d9
Joey <joseph.h.ray@gmail.com>
2018-06-11 20:35:06 +1000

    rails is a lightweight framework

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,10 @@
 class PostsController < ApplicationController
 
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def index
         @posts = Post.all
-    end
-
-    def new
-        @post = Post.new
-    end
-
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to :posts
-        else
-            render action: :new
-        end
-    end
-
-    def edit
-        @post = Post.find(params[:id])
-    end
-
-    def update
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-            redirect_to :posts
-        else
-            render action: :edit
-        end
-    end
-
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to :posts
-    end
-
-    private
-    def post_params
-        params.require(:post).permit(:id, :title, :subtitle, :content, :published, :publish_date)
     end
 end
