24d12dab9b1d33e3f000807f48106a12f40d957a
Joey <joseph.h.ray@gmail.com>
2018-06-11 21:12:01 +1000

    signup and login works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts
+  namespace :admin do
+    resources :posts
+  end
+  get "/admin", to: "admin/posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
