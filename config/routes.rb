5e87811d7c2a6dc8b55ab4748ca3152b8dae7761
Joey Ray <joseph.h.ray@gmail.com>
2018-06-14 22:20:12 +1000

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'memes/posts#index'
-  get '/memes', to: 'memes/posts#index'
+  root to: 'posts#index'
   resources :posts
   namespace :admin do
     resources :posts
   end
-  get "/memes/admin", to: "memes/admin/posts#index"
+  get "/admin", to: "admin/posts#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
