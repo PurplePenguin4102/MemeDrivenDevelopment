ad7b73efb7d2f177830c253f57dedc14caf4aa3a
Joey <joseph.h.ray@gmail.com>
2018-06-15 16:00:40 +1000

    rerouting devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
     include Pundit
     protect_from_forgery prepend: true
+
+    def after_sign_in_path_for(resource_or_scope)
+        path = super(resource_or_scope)
+        "#{config.full_host}#{path}"
+    end
 end
