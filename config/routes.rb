32065a9e80c0f13e45c5b985eb2189cc42a63dde
Joey Ray <joseph.h.ray@gmail.com>
2018-06-19 00:51:23 +1000

    herg

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   #devise_for :users
   devise_for :users, skip: [:sessions, :registrations]
   as :user do
-    get 'memes/signin', to: 'devise/sessions#new', as: :new_user_session
-    post 'memes/signin', to: 'devise/sessions#create', as: :user_session
-    delete 'memes/signout', to: 'devise/sessions#destroy', as: :destroy_user_session
-    get 'memes/signup', to: 'devise/registrations#new', as: :new_user_registration
-    post 'memes/signup', to: 'devise/registrations#new', as: :user_registration
+    get 'signin', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
+    post 'signup', to: 'devise/registrations#new', as: :user_registration
   end
   root to: 'posts#index'
   resources :posts
