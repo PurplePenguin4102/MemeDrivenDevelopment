966f066d75d6c802fda38f15cf2955e8613d686c
Joey <joseph.h.ray@gmail.com>
2018-06-19 13:13:14 +1000

    figured out my junior mistake

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     post '/signin', to: 'devise/sessions#create', as: :user_session
     delete '/signout', to: 'devise/sessions#destroy', as: :destroy_user_session
     get '/signup', to: 'devise/registrations#new', as: :new_user_registration
-    post '/signup', to: 'devise/registrations#new', as: :user_registration
+    post '/signup', to: 'devise/registrations#create', as: :user_registration
   end
   root to: 'posts#index'
   resources :posts
