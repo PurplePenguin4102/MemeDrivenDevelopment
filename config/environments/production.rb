10975962479d044d54437c41d6c01110cb659991
Joey Ray <joseph.h.ray@gmail.com>
2018-06-20 19:58:00 +1000

    assets path prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_controller.relative_url_root = "/memes"
+  config.assets.prefix = '/memes/assets'
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
