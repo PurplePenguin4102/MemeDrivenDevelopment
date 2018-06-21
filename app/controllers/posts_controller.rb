f0229bf34523146a6517dd5bfb0b7c3bc370f6c0
Joey Ray <joseph.h.ray@gmail.com>
2018-06-22 01:35:01 +1000

    sideview

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,12 @@ require 'date'
 class PostsController < ApplicationController
 
     def show
-        @post = Post.find(params[:id])
+        @post = Post.find(params[:id]).decorate
+        @side_view = Post.published
     end
 
     def index
         @posts = Post.published.decorate
+        @side_view = Post.published
     end
 end
