b739f9080e9af1adaa1d5e1dd73b6b79e3677bd1
Joey Ray <joseph.h.ray@gmail.com>
2018-07-01 14:20:39 +1000

    Jjjjjjjjjjjjjjjjjjjjjjavascript

--- app/assets/javascripts/adminArea.js
+++ app/assets/javascripts/adminArea.js
@@ -2,7 +2,9 @@ $(function () {
     $('input[type="checkbox"]').change(function() { 
         if($(this).is(":checked")) { 
             var d = new Date(); 
-            $('#post_publish_date').val(d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate());
+            var month = d.getMonth() + 1;
+            var datestr = "" + d.getFullYear() + "-" + month + "-" + d.getDate();
+            $('#post_publish_date').val(datestr);
         } 
     });
 })
